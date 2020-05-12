62c7acfc62b6a0eddf9186cf03a59bcb5990c445
joann011 <joann011@naver.com>
2020-05-12 16:03:37 +0900

    "board.php 수정"

--- php/board.php
+++ php/board.php
@@ -292,7 +292,6 @@
 
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">WRITE</h4>
         </div>
 
         <div class="modal-body">
